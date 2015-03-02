501ff345a8917b4b348b22284bb124e93b75ecb5
J.D. Garza <jdgarza22@yahoo.com>
2015-03-02 14:37:48 -0600

    updated the jquery_faq.html file to incorporate more functionality

--- public/Jquery Exercises/jquery_faq.js
+++ public/Jquery Exercises/jquery_faq.js
@@ -1,3 +1,11 @@
 $('dt').click(function() {
 $('dd').toggleClass('invisible');
 });
+
+$('ul').each(function() {
+	$(this).children().first().css('font-weight','bold')
+})
+
+$('li').click(function() {
+	$(this).parent().addClass('invisible')
+})
